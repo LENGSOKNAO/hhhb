c9246eef0f2b5a77e390840af2a11754f2976e08
cool boy <soknao2003@gmail.com>
2024-09-02 10:44:37 +0700

    add

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import l from "../assets/long_icon.svg"
+
+
+const Navbar = () => {
+  return (
+    <>
+      <nav>
+        <div className="">
+          <div className="">
+            <div className=""></div>
+
+            <img src={l} alt="" />
+          </div>
+
+          <div className="">
+            <ul>
+              <li></li>
+              <li>Sign in</li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
